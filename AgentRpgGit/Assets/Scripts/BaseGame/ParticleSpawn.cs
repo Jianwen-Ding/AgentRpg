17ff4c055993435a9608ad0018588914c8ca802d
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2022-02-10 22:01:39 -0500

    added background

--- AgentRpgGit/Assets/Scripts/BaseGame/ParticleSpawn.cs
+++ AgentRpgGit/Assets/Scripts/BaseGame/ParticleSpawn.cs
@@ -15,6 +15,12 @@ public class ParticleSpawn : MonoBehaviour
     [SerializeField]
     int amount;
     [SerializeField]
+    float timeSpawn;
+    [SerializeField]
+    float timeSpawnLeft = 0;
+    [SerializeField]
+    bool spawnsOverTime;
+    [SerializeField]
     float XSpeedMin;
     [SerializeField]
     float XSpeedMax;
@@ -52,6 +58,36 @@ public class ParticleSpawn : MonoBehaviour
                 Object.GetComponent<SpriteRenderer>().color = newColor; 
             }
             Object.transform.parent = gameObject.transform;
+        }
+    }
+    public void Update()
+    {
+        if(spawnsOverTime == true)
+        {
+            if(timeSpawnLeft < 0)
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    GameObject Object = Instantiate(prefab, new Vector3(Random.Range(XMin + gameObject.transform.position.x, XMax + gameObject.transform.position.x), Random.Range(YMin + gameObject.transform.position.y, YMax + gameObject.transform.position.y), 0), Quaternion.identity.normalized);
+                    if (Object.GetComponent<Rigidbody2D>() != null)
+                    {
+                        Object.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(XSpeedMin, XSpeedMax), Random.Range(YSpeedMin, YSpeedMax));
+                        Object.GetComponent<Rigidbody2D>().angularVelocity = Random.Range(RotationSpeedMin, RotationSpeedMax);
+                    }
+                    if (Object.GetComponent<SpriteRenderer>() != null && nonWhite == false)
+                    {
+                        float colorSet = Random.Range(darkest, lightest);
+                        Color newColor = new Color(colorSet, colorSet, colorSet);
+                        Object.GetComponent<SpriteRenderer>().color = newColor;
+                    }
+                    Object.transform.parent = gameObject.transform;
+                }
+                timeSpawnLeft = timeSpawn;
+            }
+            else
+            {
+                timeSpawnLeft -= Time.deltaTime;
+            }
         }
     }
 }
