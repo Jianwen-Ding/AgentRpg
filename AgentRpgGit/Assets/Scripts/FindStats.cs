f20634788d58d3f042f4f84ff4555a7c3b29e5ff
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2022-03-17 21:13:03 -0400

    3/17/2022(2)

--- AgentRpgGit/Assets/Scripts/FindStats.cs
+++ AgentRpgGit/Assets/Scripts/FindStats.cs
@@ -9,6 +9,7 @@ public class FindStats : MonoBehaviour
     public GameObject Speed;
     public GameObject Defense;
     public GameObject Attack;
+    public GameObject MaxHealth;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class FindStats : MonoBehaviour
         Speed.GetComponent<TMPro.TextMeshProUGUI>().text = "Speed: " + g.Speed[findIndex];
         Defense.GetComponent<TMPro.TextMeshProUGUI>().text = "Defense: " + g.Defense[findIndex];
         Attack.GetComponent<TMPro.TextMeshProUGUI>().text = "Attack: " + g.Damage[findIndex];
+        MaxHealth.GetComponent<TMPro.TextMeshProUGUI>().text = "Max Health: " + g.MaxHealth[findIndex];
     }
 
     // Update is called once per frame
---
+++ AgentRpgGit/Assets/Scripts/glossaryManage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glossaryManage : MonoBehaviour
+{
+    [SerializeField]
+    string[] text;
+    [SerializeField]
+    GameObject textPrefab;
+    [SerializeField]
+    float GridStartx;
+    [SerializeField]
+    float GridStarty;
+    [SerializeField]
+    float Changey;
+    [SerializeField]
+    float MinChangeMaxY;
+    [SerializeField]
+    GameObject GameObjectAllAttach;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int MovesLoaded = 0;
+            while(MovesLoaded < text.Length)
+            {
+                GameObject GameObjectLoaded = Instantiate(textPrefab, new Vector3(GridStartx , GridStarty + Changey * MovesLoaded, 0), Quaternion.identity.normalized);
+                GameObjectLoaded.GetComponent<TMPro.TextMeshProUGUI>().text = text[MovesLoaded];
+                GameObjectLoaded.transform.parent = GameObjectAllAttach.transform;
+            MovesLoaded++;
+            }
+        GameObjectAllAttach.GetComponent<PickMoveSlide>().MinY = 0;
+        GameObjectAllAttach.GetComponent<PickMoveSlide>().MaxY = -Changey * MovesLoaded + MinChangeMaxY;
+        //???????????????????????????????
+    }
+}
